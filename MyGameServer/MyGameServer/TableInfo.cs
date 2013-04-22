036f5b9e481859f75064d8a9476a258b3ed12e42
LM <idealmost@gmail.com>
2013-04-22 22:21:36 +0800

    sss

---
+++ MyGameServer/MyGameServer/TableInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGameServer
+{
+    class TableInfo
+    {
+        public bool[] seats;
+        public TableInfo()
+        {
+            seats = new bool[4]{true,true,true,true};//初始化都能坐
+            
+        }
+    }
+}
