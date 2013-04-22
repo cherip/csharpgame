188f6e679e6e7e11dd0898198aaf32a0127fdce9
LM <idealmost@gmail.com>
2013-04-22 23:14:33 +0800

    sss

--- MyGameServer/MyGameServer/TableInfo.cs
+++ MyGameServer/MyGameServer/TableInfo.cs
@@ -8,10 +8,11 @@ namespace MyGameServer
     class TableInfo
     {
         public bool[] seats;
+        public bool tabelEable;//游戏如果已经开始，就不激活桌子了
         public TableInfo()
         {
             seats = new bool[4]{true,true,true,true};//初始化都能坐
-            
+            tabelEable = true;
         }
     }
 }
